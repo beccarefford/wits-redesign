2a2e7b3f163fad8e80c4d070133c3fca7e65c9f6
Becca Refford <beccarefford@gmail.com>
2016-07-22 10:06:15 -0400

    unhiding keynotes for raleigh

--- wp-content/themes/WITS/home-raleigh.php
+++ wp-content/themes/WITS/home-raleigh.php
@@ -4,40 +4,14 @@
 
 <?php get_header(); ?>
 
-<section class="container banner">
-<div class="landing" style="background-image:
-	url('/wp-content/uploads/2015/11/Philadelphia-Skyline-ETE.jpg')">
-		<h1>Raleigh</h1>
-		<div class="row">
-			<div class="col-md-12">
-				<div class="box">
-					<div>
-						<!--<span class="sub">WHEN</span>-->
-						<span class="title"></span> <span class="desc">April 9, 2016</span>
-					</div>
-				</div>
+	<section class="container banner">
+	<div class="landing" style="background-image:
+    url('/wp-content/uploads/2016/07/Screen-Shot-2016-07-19-at-3.09.44-PM.png')">
+			<div class="landing-home">
+				<h1>Philadelphia</h1>
 			</div>
 		</div>
-	</div>
-</section>
-
-	<section class="container headline">
-		<div class="row">
-			<div class="col-md-7">
-				<h2>Inspire. Educate. Connect.</h2>
-				<p>The Women in Tech Summit inspires, educates and connects
-					women in the technology industry. Join us for a unique combination of deep-dive, hands-on tech
-				workshops; information and discussions about careers in tech and
-				how to pursue them; and connection and networking opportunities with
-				other women in various aspects and careers in technology.</p>
-			</div>
-			<div class="col-md-5">
-				<a target="_blank" href="http://chariotsolutions.com">
-				<img class="left" src="<?php echo get_stylesheet_directory_uri(); ?>/images/chariot-presented-by.jpg"
-			alt="Presented by Chariot Solutions" /></a>
-			</div>
-		</div>
-        </section>
+	</section>
 
 	<section class="container keynote">
         <h2>Keynote Speakers</h2>
@@ -45,7 +19,7 @@
 		<?php
 wp_reset_postdata();
 $keynote = array(
-			'post_type' => 'raleigh_speaker',
+			'post_type' => 'philly_speaker',
 			'orderby' => 'date',
 			'order' => 'DESC',
 			'key' => 'keynote',
@@ -57,7 +31,7 @@ $keynote = array(
 $myquery = new WP_Query($keynote);
 	if( $myquery->have_posts() ) :
 		while( $myquery->have_posts() ) : $myquery->the_post();
-			if (get_field('raleigh_keynote')) { ?>
+			if (get_field('philly_keynote')) { ?>
 		<div class="col-md-6">
 			<div class="col-md-5">
 				<center>
@@ -88,7 +62,7 @@ wp_reset_postdata(); ?>
 <div class="container">
 <?php
     $loop_speakers = new WP_Query( array(
-    'post_type' => 'raleigh_speaker',
+    'post_type' => 'philly_speaker',
     'posts_per_page' => -1,
     'orderby' => 'menu_order',
     'order' => 'DESC'
@@ -142,7 +116,7 @@ wp_reset_postdata(); ?>
 
 
 
-<!-- Start Sponsors -->
+<?php /*!-- Start Sponsors -->
 <div id="tile_sponsors" class="container widget">
 	<h2><a href="/sponsor">Sponsors</a></h2>
 	<a href="/sponsor" class="btn btn-primary btn-header pull-right hidden-xs"> View All Sponsors </a> <br/>
@@ -162,10 +136,10 @@ wp_reset_postdata(); ?>
 			</div>
 
 	<!-- .sponsors .sponsors-lg -->
-	<div class="text-center visible-xs">
+	<?php /* <div class="text-center visible-xs">
 		<a href="/sponsors" class="btn btn-primary btn-header"> View All Sponsors </a>
 	</div>
 </div>
-<!-- End Sponsors -->
+<!-- End Sponsors --> */ ?>
 
 <?php get_footer(); ?>
