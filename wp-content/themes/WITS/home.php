d5fa0c34570acd1204611ebaab915468d3d46986
Becca Refford <beccarefford@gmail.com>
2016-07-24 21:44:43 -0400

    adding mailchimp signup list to home page

--- wp-content/themes/WITS/home.php
+++ wp-content/themes/WITS/home.php
@@ -211,7 +211,7 @@ wp_reset_postdata(); ?>
 
 <section class="container headline">
 	<div class="row">
-		<div class="col-md-7">
+		<div class="col-md-6">
 			<h2>Inspire. Educate. Connect.</h2>
 			<p>The Women in Tech Summit inspires, educates and connects
 				women in the technology industry. Join us for a unique combination of deep-dive, hands-on tech
@@ -219,8 +219,34 @@ wp_reset_postdata(); ?>
 			how to pursue them; and connection and networking opportunities with
 			other women in various aspects and careers in technology.</p>
 		</div>
-		<div class="col-md-5">
-			&nbsp;
+		<div class="col-md-6">
+			<!-- Begin MailChimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="//womenintechsummit.us4.list-manage.com/subscribe/post?u=cd7267f1d913ce62c1b8f5b7b&amp;id=af700128b7" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	<h2>Subscribe to our mailing list to stay up-to-date with future events.</h2>
+	&nbsp;
+<div class="mc-field-group">
+	<label for="mce-EMAIL">Email Address </label>
+	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+</div>
+	<div id="mce-responses" class="clear">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_cd7267f1d913ce62c1b8f5b7b_af700128b7" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='MMERGE3';ftypes[3]='text';fnames[4]='MMERGE4';ftypes[4]='text';fnames[5]='MMERGE5';ftypes[5]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+<!--End mc_embed_signup-->
 		</div>
 	</div>
 </section>
