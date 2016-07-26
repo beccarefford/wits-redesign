3219c02e9eb9697d6abe60b98849bb102021abfe
Becca Refford <beccarefford@gmail.com>
2016-07-26 11:10:51 -0400

    philly single view panel working

--- wp-content/themes/WITS/single-philly_session.php
+++ wp-content/themes/WITS/single-philly_session.php
@@ -5,44 +5,10 @@
 <section class="container">
 
   <div class="row">
-    <div class="col-md-2 col-xs-4">
-    <?php $pics = get_posts(array(
-      'post_type' => 'philly_session',
-      'meta_query' => array(
-        array(
-          'key' => 'speaker_photo', // name of custom field
-          'value' => '"' . get_the_ID() . '"', // matches exaclty "123", not just 123. This prevents a match for "1234"
-          'compare' => 'LIKE'
-        )
-      )
-    )); ?>
-
-    <?php $pics = get_field('philly_speaker_photo');
-      if ($pics): ?>
-        <?php foreach ($pics as $post): ?>
-          <?php setup_postdata($post); ?>
-            <a href="<?php the_permalink(); ?>">
-              <div class="circular-image-archive">
-                <?php echo get_the_post_thumbnail($post->ID, 'medium'); ?>
-              </div>
-            </div>
-
-    <div class="col-md-10 col-xs-8">
-        <h2><?php echo the_title(); ?></h2></a>
-      <div class="speaker-job-title">
-          <?php echo the_field('philly_job_title'); ?>
-      <?php endforeach; ?>
-    <?php endif; ?>
-      <?php wp_reset_postdata(); ?>
-      </div>
-
-    </div>
-  </div>
-
-  <div class="row">
     <div class="col-md-12 col-xs-12">
       <h2><?php echo the_title(); ?></h2>
       <?php echo the_content(); ?>
+
       <?php /* <br /><strong>Tags: </strong><?php the_terms($post->ID, 'tags'); ?>
 
       <?php //Display Location Category ?>
@@ -79,7 +45,41 @@
       <?php endif; ?>
     </div>
   </div>
-</div>
+
+  <?php $pics = get_posts(array(
+    'post_type' => 'philly_session',
+    'meta_query' => array(
+      array(
+        'key' => 'speaker_photo', // name of custom field
+        'value' => '"' . get_the_ID() . '"', // matches exaclty "123", not just 123. This prevents a match for "1234"
+        'compare' => 'LIKE'
+      )
+    )
+  )); ?>
+
+  <?php $pics = get_field('philly_speaker_photo');
+    if ($pics): ?>
+      <?php foreach ($pics as $post): ?>
+        <?php setup_postdata($post); ?>
+        <div class="row">
+          <div class="col-md-2 col-xs-4">
+          <a href="<?php the_permalink(); ?>">
+            <div class="circular-image-archive">
+              <?php echo get_the_post_thumbnail($post->ID, 'medium'); ?>
+            </div>
+          </div>
+
+        <div class="col-md-10 col-xs-8">
+          <h2><?php echo the_title(); ?></h2></a>
+            <div class="speaker-job-title">
+              <?php echo the_field('philly_job_title'); ?>
+                <?php wp_reset_postdata(); ?>
+          </div>
+        </div>
+      </div>
+    <?php endforeach; ?>
+  <?php endif; ?>
+  
 </section>
 
 <?php endwhile; ?>
