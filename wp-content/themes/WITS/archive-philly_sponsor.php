3494568c0a1f379d75d55fc2ae8156339059ca9f
Becca Refford <beccarefford@gmail.com>
2016-08-27 16:47:18 -0400

    fixing formatting changes to philly sponsor page

--- wp-content/themes/WITS/archive-philly_sponsor.php
+++ wp-content/themes/WITS/archive-philly_sponsor.php
@@ -37,10 +37,14 @@ $myquery = new WP_Query($headline);
 
           <div class="col-md-6 col-xs-12">
             <h3><?php the_title(); ?></h3>
-              <a target="_blank" href="<?php the_field('philly_sponsor_url')?>">
-                <?php echo the_field('philly_sponsor_url'); ?></a>
-              <br /><?php if( get_field('philly_sponsor_twitter') ): ?>
-              <a href="<?php the_field('philly_sponsor_twitter');?>"><i class="icon-twitter icon-2x"></i></a>
+            <br />
+              <ul>
+                <li><a target="_blank" href="<?php the_field('philly_sponsor_url')?>">
+                <?php echo the_field('philly_sponsor_url'); ?></a></li>
+              <li><?php if( get_field('philly_sponsor_twitter') ): ?>
+              <a href="<?php the_field('philly_sponsor_twitter');?>">
+              <?php echo the_field('philly_sponsor_url'); ?></a></li>
+            </ul>
             <?php endif; ?>
           </div>
         </div>
