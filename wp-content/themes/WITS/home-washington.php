d3d3f42dadedee9410cc53380a99e9f3b965e1ee
Becca Refford <beccarefford@gmail.com>
2016-08-28 20:38:00 -0400

    making schedule button open in new tab

--- wp-content/themes/WITS/home-washington.php
+++ wp-content/themes/WITS/home-washington.php
@@ -14,7 +14,7 @@
 					<span class="desc">Spring 2017</span>
 					<?php if( get_field('washington_schedule_button_url') ): ?>
 						<div>
-							<a href="<?php the_field('washington_schedule_button_url')?>"
+							<a target="_blank" href="<?php the_field('washington_schedule_button_url')?>"
 								class="btn btn-primary btn-header">View Schedule</a>
 						</div>
 					<?php endif; ?>
