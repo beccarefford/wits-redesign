64b5b6990bf3972066212ebd71d995611d732a23
Becca Refford <beccarefford@gmail.com>
2016-12-29 21:43:08 -0500

    adding conditional summit series

--- wp-content/themes/WITS/archive-philly_sponsor.php
+++ wp-content/themes/WITS/archive-philly_sponsor.php
@@ -9,9 +9,6 @@ get_header(); ?>
 <?php
   $current_post = 1; ?>
 
-  <?php // sponsor_type queries
-        // begin with 'Headline'?>
-
   <?php $headline = array(
         	'post_type' => 'summitseries_sponsor',
         	'orderby' => 'date',
@@ -42,6 +39,53 @@ $myquery = new WP_Query($headline);
               <li><?php if( get_field('summit_series_sponsor_twitter') ): ?>
               <a href="<?php the_field('summit_series_sponsor_twitter');?>">
               <?php echo the_field('summit_series_sponsor_twitter'); ?></a></li>
+            </ul>
+            <?php endif; ?>
+          </div>
+        </div>
+      <hr />
+    <?php
+  endwhile;
+endif;
+?>
+
+<?php wp_reset_postdata(); ?>
+
+  <?php // sponsor_type queries
+        // begin with 'Headline'?>
+
+  <?php $headline = array(
+        	'post_type' => 'philly_sponsor',
+        	'orderby' => 'date',
+        	'order' => 'ASC',
+        	'meta_key' => 'philly_sponsor_level',
+        	'meta_value' => 'Headline',
+        	);
+
+$myquery = new WP_Query($headline);
+  if( $myquery->have_posts() ) :
+    while( $myquery->have_posts() ) : $myquery->the_post();
+        $current_post = $myquery->current_post + 1; ?>
+
+        <?php if($current_post == 1) { ?>
+          <h3 class="sponsor"><span>Headline</span></h3>
+        <?php } ?>
+        <div class="row">
+          <div class="col-md-6 col-xs-12">
+            <a target="_blank" href="<?php the_field('philly_sponsor_url')?>">
+              <?php echo get_the_post_thumbnail($post->ID, 'large'); ?>
+            </a>
+          </div>
+
+          <div class="col-md-6 col-xs-12">
+            <h3><?php the_title(); ?></h3>
+            <br />
+              <ul>
+                <li><a target="_blank" href="<?php the_field('philly_sponsor_url')?>">
+                <?php echo the_field('philly_sponsor_url'); ?></a></li>
+              <li><?php if( get_field('philly_sponsor_twitter') ): ?>
+              <a href="<?php the_field('philly_sponsor_twitter');?>">
+              <?php echo the_field('philly_sponsor_twitter'); ?></a></li>
             </ul>
             <?php endif; ?>
           </div>
@@ -323,7 +367,6 @@ endif;
         'post_type' => 'philly_sponsor',
         'orderby' => 'date',
         'order' => 'ASC',
-        'post_status' => 'published',
         'meta_key' => 'philly_sponsor_level',
         'meta_value' => 'Media',
         );
