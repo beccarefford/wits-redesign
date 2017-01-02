fb90a3db250567e88dc0ff54c14bcdd9b09109b0
Becca Refford <beccarefford@gmail.com>
2017-01-02 09:05:55 -0500

    adding main sponsor page

---
+++ wp-content/themes/WITS/all-sponsors.php
@@ -0,0 +1,294 @@
+<?php
+
+/** Template Name: All Sponsors */
+
+get_header(); ?>
+
+<section class="container banner">
+<div class="landing" style="background-image:
+  url('/wp-content/uploads/2017/01/Screen-Shot-2017-01-02-at-8.58.13-AM.png')">
+    <div class="landing-home">
+      <h1>All Sponsors</h1>
+    </div>
+</section>
+
+<section class="container">
+
+<?php /* SUMMIT SERIES */ ?>
+
+<?php
+  $current_post = 1; ?>
+
+  <?php $query = array(
+        	'post_type' => 'summitseries_sponsor',
+        	'orderby' => 'date',
+        	'order' => 'ASC',
+        	);
+
+$myquery = new WP_Query($query);
+  if( $myquery->have_posts() ) :
+    while( $myquery->have_posts() ) : $myquery->the_post();
+        $current_post = $myquery->current_post + 1; ?>
+
+        <?php if($current_post == 1) { ?>
+          <h3 class="sponsor"><span>Summit Series</span></h3>
+        <?php } ?>
+        <div class="row">
+          <div class="col-md-6 col-xs-12">
+            <a target="_blank" href="<?php the_field('summit_series_sponsor_url')?>">
+              <?php echo get_the_post_thumbnail($post->ID, 'large'); ?>
+            </a>
+          </div>
+
+          <div class="col-md-6 col-xs-12">
+            <h3><?php the_title(); ?></h3>
+            <br />
+              <ul>
+                <li><a target="_blank" href="<?php the_field('summit_series_sponsor_url')?>">
+                <?php echo the_field('summit_series_sponsor_url'); ?></a></li>
+              <li><?php if( get_field('summit_series_sponsor_twitter') ): ?>
+              <a href="<?php the_field('summit_series_sponsor_twitter');?>">
+              <?php echo the_field('summit_series_sponsor_twitter'); ?></a></li>
+            </ul>
+            <?php endif; ?>
+          </div>
+        </div>
+      <hr />
+    <?php
+  endwhile;
+endif;
+?>
+
+<?php wp_reset_postdata(); ?>
+
+<?php /* PHILADELPHIA SPONSORS */ ?>
+
+  <?php // sponsor_type queries
+        // begin with 'Headline'?>
+
+  <?php $query = array(
+        	'post_type' => 'philly_sponsor',
+        	'orderby' => 'date',
+        	'order' => 'ASC',
+        	'meta_key' => 'philly_sponsor_level',
+        	'meta_value' => 'PremierCitySponsor',
+        	);
+
+$myquery = new WP_Query($query);
+  if( $myquery->have_posts() ) :
+    while( $myquery->have_posts() ) : $myquery->the_post();
+        $current_post = $myquery->current_post + 1; ?>
+
+        <?php if($current_post == 1) { ?>
+          <h3 class="sponsor"><span>Premier City Sponsor</span></h3>
+        <?php } ?>
+        <div class="row">
+          <div class="col-md-6 col-xs-12">
+            <a target="_blank" href="<?php the_field('philly_sponsor_url')?>">
+              <?php echo get_the_post_thumbnail($post->ID, 'large'); ?>
+            </a>
+          </div>
+
+          <div class="col-md-6 col-xs-12">
+            <h3><?php the_title(); ?></h3>
+            <br />
+              <ul>
+                <li><a target="_blank" href="<?php the_field('philly_sponsor_url')?>">
+                <?php echo the_field('philly_sponsor_url'); ?></a></li>
+              <li><?php if( get_field('philly_sponsor_twitter') ): ?>
+              <a href="<?php the_field('philly_sponsor_twitter');?>">
+              <?php echo the_field('philly_sponsor_twitter'); ?></a></li>
+            </ul>
+            <?php endif; ?>
+          </div>
+        </div>
+      <hr />
+    <?php
+  endwhile;
+endif;
+?>
+
+<?php wp_reset_postdata(); ?>
+
+<center>
+  <h1>Philadelphia Sponsors</h1>
+</center>
+
+<?php $query = array(
+        'post_type' => 'philly_sponsor',
+        'orderby' => 'date',
+        'order' => 'ASC',
+        'meta_key' => 'philly_sponsor_level',
+        'meta_value' => 'CitySponsor',
+        );
+
+$myquery = new WP_Query($query);
+if( $myquery->have_posts() ) :
+  while( $myquery->have_posts() ) : $myquery->the_post();
+      $current_post = $myquery->current_post + 1; ?>
+
+      <?php if($current_post == 1) { ?>
+        <h3 class="sponsor"><span>City Sponsor</span></h3>
+      <?php } ?>
+      <div class="row">
+        <div class="col-md-6 col-xs-12">
+          <a target="_blank" href="<?php the_field('philly_sponsor_url')?>">
+            <?php echo get_the_post_thumbnail($post->ID, 'large'); ?>
+          </a>
+        </div>
+
+        <div class="col-md-6 col-xs-12">
+          <h3><?php the_title(); ?></h3>
+          <br />
+            <ul>
+              <li><a target="_blank" href="<?php the_field('philly_sponsor_url')?>">
+              <?php echo the_field('philly_sponsor_url'); ?></a></li>
+            <li><?php if( get_field('philly_sponsor_twitter') ): ?>
+            <a href="<?php the_field('philly_sponsor_twitter');?>">
+            <?php echo the_field('philly_sponsor_twitter'); ?></a></li>
+          </ul>
+          <?php endif; ?>
+        </div>
+      </div>
+    <hr />
+  <?php
+endwhile;
+endif;
+?>
+
+<?php wp_reset_postdata(); ?>
+
+
+<?php $query = array(
+        'post_type' => 'philly_sponsor',
+        'orderby' => 'date',
+        'order' => 'ASC',
+        'meta_key' => 'philly_sponsor_level',
+        'meta_value' => 'Friends',
+        );
+
+$myquery = new WP_Query($query);
+if( $myquery->have_posts() ) :
+  while( $myquery->have_posts() ) : $myquery->the_post();
+      $current_post = $myquery->current_post + 1; ?>
+
+      <?php if($current_post == 1) { ?>
+        <h3 class="sponsor"><span>Friends</span></h3>
+      <?php } ?>
+      <div class="row">
+        <div class="col-md-6 col-xs-12">
+          <a target="_blank" href="<?php the_field('philly_sponsor_url')?>">
+            <?php echo get_the_post_thumbnail($post->ID, 'large'); ?>
+          </a>
+        </div>
+
+        <div class="col-md-6 col-xs-12">
+          <h3><?php the_title(); ?></h3>
+          <br />
+            <ul>
+              <li><a target="_blank" href="<?php the_field('philly_sponsor_url')?>">
+              <?php echo the_field('philly_sponsor_url'); ?></a></li>
+            <li><?php if( get_field('philly_sponsor_twitter') ): ?>
+            <a href="<?php the_field('philly_sponsor_twitter');?>">
+            <?php echo the_field('philly_sponsor_twitter'); ?></a></li>
+          </ul>
+          <?php endif; ?>
+        </div>
+      </div>
+    <hr />
+  <?php
+endwhile;
+endif;
+?>
+
+<?php wp_reset_postdata(); ?>
+
+<?php $query = array(
+        'post_type' => 'philly_sponsor',
+        'orderby' => 'date',
+        'order' => 'ASC',
+        'meta_key' => 'philly_sponsor_level',
+        'meta_value' => 'MediaAndPartners',
+        );
+
+$myquery = new WP_Query($query);
+if( $myquery->have_posts() ) :
+  while( $myquery->have_posts() ) : $myquery->the_post();
+      $current_post = $myquery->current_post + 1; ?>
+
+      <?php if($current_post == 1) { ?>
+        <h3 class="sponsor"><span>Media & Partners</span></h3>
+      <?php } ?>
+      <div class="row">
+        <div class="col-md-6 col-xs-12">
+          <a target="_blank" href="<?php the_field('philly_sponsor_url')?>">
+            <?php echo get_the_post_thumbnail($post->ID, 'large'); ?>
+          </a>
+        </div>
+
+        <div class="col-md-6 col-xs-12">
+          <h3><?php the_title(); ?></h3>
+          <br />
+            <ul>
+              <li><a target="_blank" href="<?php the_field('philly_sponsor_url')?>">
+              <?php echo the_field('philly_sponsor_url'); ?></a></li>
+            <li><?php if( get_field('philly_sponsor_twitter') ): ?>
+            <a href="<?php the_field('philly_sponsor_twitter');?>">
+            <?php echo the_field('philly_sponsor_twitter'); ?></a></li>
+          </ul>
+          <?php endif; ?>
+        </div>
+      </div>
+    <hr />
+  <?php
+endwhile;
+endif;
+?>
+
+<?php wp_reset_postdata(); ?>
+
+<?php $query = array(
+        'post_type' => 'philly_sponsor',
+        'orderby' => 'date',
+        'order' => 'ASC',
+        'meta_key' => 'philly_sponsor_level',
+        'meta_value' => 'Snack',
+        );
+
+$myquery = new WP_Query($query);
+if( $myquery->have_posts() ) :
+  while( $myquery->have_posts() ) : $myquery->the_post();
+      $current_post = $myquery->current_post + 1; ?>
+
+      <?php if($current_post == 1) { ?>
+        <h3 class="sponsor"><span>Snack</span></h3>
+      <?php } ?>
+      <div class="row">
+        <div class="col-md-6 col-xs-12">
+          <a target="_blank" href="<?php the_field('philly_sponsor_url')?>">
+            <?php echo get_the_post_thumbnail($post->ID, 'large'); ?>
+          </a>
+        </div>
+
+        <div class="col-md-6 col-xs-12">
+          <h3><?php the_title(); ?></h3>
+          <br />
+            <ul>
+              <li><a target="_blank" href="<?php the_field('philly_sponsor_url')?>">
+              <?php echo the_field('philly_sponsor_url'); ?></a></li>
+            <li><?php if( get_field('philly_sponsor_twitter') ): ?>
+            <a href="<?php the_field('philly_sponsor_twitter');?>">
+            <?php echo the_field('philly_sponsor_twitter'); ?></a></li>
+          </ul>
+          <?php endif; ?>
+        </div>
+      </div>
+    <hr />
+  <?php
+endwhile;
+endif;
+?>
+
+<?php wp_reset_postdata(); ?>
+
+<?php get_footer(); ?>
