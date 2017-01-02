df961b98e8fc7e47ef9e77a128599160cd74de2d
Becca Refford <beccarefford@gmail.com>
2017-01-02 11:30:01 -0500

    adding dates and removing 2016 from raleigh home page

--- wp-content/themes/WITS/home-raleigh.php
+++ wp-content/themes/WITS/home-raleigh.php
@@ -28,7 +28,7 @@
 <a href="/raleigh_session">
 	<div class="col-md-4 nopadding">
 			<div style="max-height:70px;" class="home-session" onmouseover="this.style.background='#2A2329';" onmouseout="this.style.background='#1D181C';">
-				<h2>2016 Sessions</h2>
+				<h2>Sessions</h2>
 			</div>
 	</div>
 </a>
@@ -36,7 +36,7 @@
 <a href="/raleigh_speaker">
 	<div class="col-md-4 nopadding">
 		<div style="max-height:70px;" class="home-speaker" onmouseover="this.style.background='#493D48';" onmouseout="this.style.background='#3C323B';">
-			<h2>2016 Speakers</h2>
+			<h2>Speakers</h2>
 		</div>
 	</div>
 </a>
@@ -44,7 +44,7 @@
 <a href="/raleigh-sponsors">
 	<div class="col-md-4 nopadding">
 		<div style="max-height:70px;" class="home-sponsor" onmouseover="this.style.background='#635B63';" onmouseout="this.style.background='#493D48';">
-			<h2>2016 Sponsors</h2>
+			<h2>Sponsors</h2>
 		</div>
 	</div>
 </a>
@@ -65,7 +65,7 @@
 				&nbsp;
 				<h2>Event Details</h2>
 				<p><strong>Date:</strong> November 11, 2017
-				<br /><strong>Venue: </strong>Location TBD
+				<br /><strong>Venue: </strong>The Frontier at Research Triangle Park
 				<br />800 Park Offices Dr, Research Triangle Park
 				<br />North Carolina, 27709
 			</div>
