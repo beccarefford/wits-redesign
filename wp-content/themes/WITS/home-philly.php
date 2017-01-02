00321aad06a65564e2856f35607f577f6da8f037
Becca Refford <beccarefford@gmail.com>
2017-01-02 08:40:48 -0500

    adding new embed code to philly

--- wp-content/themes/WITS/home-philly.php
+++ wp-content/themes/WITS/home-philly.php
@@ -76,7 +76,7 @@
 						<h2>Register for the 2017 Event</h2>
 						&nbsp;
 					</center>
-				<script src='https://www.universe.com/embed.js'></script><iframe id="unii-embed-page" src="https://www.universe.com/embed/listings/584e9209b755070036981d83" frameborder="0" scrolling="no"></iframe>
+				<script src='https://www.universe.com/embed.js'></script><iframe id="unii-embed-page" src="https://www.universe.com/embed/listings/584dd72589827c0042850a39" frameborder="0" scrolling="no"></iframe>
 				</div>
 			</div>
 
