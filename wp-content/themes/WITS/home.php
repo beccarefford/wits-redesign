03bdcd3a96e257f4a35a27251d0abc7e5a275123
Becca Refford <beccarefford@gmail.com>
2017-01-09 09:42:26 -0500

    fixing boston link on home page

--- wp-content/themes/WITS/home.php
+++ wp-content/themes/WITS/home.php
@@ -56,7 +56,8 @@
 	</div>
 
 	<div class="col-md-3">
-		<a href="/boston_home">
+		<a href="/boston-home">
+			
 		<div style="padding: 50px; margin-top: -10px; color: rgb(255, 255, 255);
 		min-height: 260px; max-height: 260px; background-color: #2A2329;" class="panel-widget-style"
 		onmouseover="this.style.background='#9E1B42';"
